e8bbb7e353b108db5377da5afb3ab39dc7c271d9
Oscar Barajas Tavares <oscar@gndx.dev>
2022-04-28 17:48:00 -0500

    :sparkles: [ADD] hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,17 @@
+
+// var nameOfDog; // undefined
+
+// console.log(nameOfDog);
+// var nameOfDog = 'Elmo';
+// console.log(nameOfDog);
+var elmo;
+function nameOfDog() {
+  console.log(`El mejor perrito es ${elmo}`);
+}
+nameOfDog();
+
+function nameOfDog() {
+  console.log(`El mejor perrito es ${elmo}`);
+}
+
+
