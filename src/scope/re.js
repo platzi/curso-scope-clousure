5f9c4cdd77b3ecea88716f6dc45f5737cfb7ddab
Oscar Barajas Tavares <oscar@gndx.dev>
2022-04-28 17:29:00 -0500

    :sparkles: [ADD] re

---
+++ src/scope/re.js
@@ -0,0 +1,19 @@
+var firstName = "Oscar";
+var firstName = "Ana";
+
+console.log(firstName);
+
+// let
+let fruit = "Apple";
+fruit = "Banana";
+let fruit = "Kiwi";
+
+console.log(fruit);
+
+
+// const
+const animal = "dog";
+aminal = "Cat";
+const animal = "Snake";
+
+console.log(animal);
